95986b812dc0c294f5c777e280122035745c9bf1
吉法师 <765715928@qq.com>
2020-07-09 19:35:48 +0800

    提交

--- main.js
+++ main.js
@@ -2,16 +2,18 @@ const applyMysql = require('./tools/applyMysql');
 const Socket = require('./tcp/socket');
 const TcpManager = require('./tcp/TcpManager');
 const Nodis = require('./Nodis/Nodis');
+
 const fs = require('fs');
 const ini = require('ini');
 const { fileName } = require('./tools/define');
 async function init() {
     let mysqlInstance = new applyMysql();
-    let socket = new Socket();
+    let socket = new Socket();  
 
-    let tcpManager = new TcpManager();
+    let tcpManager = new TcpManager();  
     let nodis = new Nodis();
     let ini = await readIni();
+    mysqlInstance.init(ini.mysql);
     console.log("配置文件的内容为",ini);
     global.instance = {
         dbHandler : mysqlInstance,
@@ -19,6 +21,7 @@ async function init() {
         tcpHandler : tcpManager,
         nodis:nodis,
         ini:ini,
+        
     };
     socket.init();
 }
